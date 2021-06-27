dba3ecb4d8b0554a75bb4c639ebe36ec1e8005de
HItesh Choudhary <hitesh@hiteshchoudhary.com>
2021-06-27 13:16:42 +0530

    Initial Commit

---
+++ LCOMockTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LCOMockTest
+//
+//  Created by lco on 27/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LCOMockTest/LCOMockTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCOMockTestApp.swift
+//  LCOMockTest
+//
+//  Created by lco on 27/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LCOMockTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
